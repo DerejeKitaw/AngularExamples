fc5e25eb2d86565c66d566a970889188c3fa6814
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-10-31 23:01:08 -0400

    home component added

--- ang/src/app/app.module.ts
+++ ang/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+  // All component need to be declared to be used in the module
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
