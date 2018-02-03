36ebb78989a6ef9cf0300bbbc2459abf94d804f5
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-02 23:48:30 -0500

    initial commit demo three

--- demoOne/src/app/_nav.ts
+++ demoOne/src/app/_nav.ts
@@ -3,131 +3,78 @@ export const navigation = [
       name: 'Dashboard',
       url: '/dashboard',
       icon: 'icon-speedometer',
-      badge: {
-        variant: 'info',
-        text: 'NEW'
-      }
+      // badge: {
+      //   variant: 'info',
+      //   text: 'NEW'
+      // }
     },
     {
       title: true,
-      name: 'UI elements'
+      name: 'Eqipments'
     },
     {
-      name: 'Components',
+      name: 'Add Equipmaent',
       url: '/components',
       icon: 'icon-puzzle',
       children: [
         {
-          name: 'Buttons',
+          name: 'Inverter',
           url: '/components/buttons',
           icon: 'icon-puzzle'
         },
         {
-          name: 'Social Buttons',
+          name: 'Modules',
           url: '/components/social-buttons',
           icon: 'icon-puzzle'
         },
         {
-          name: 'Cards',
+          name: 'Service Type',
           url: '/components/cards',
           icon: 'icon-puzzle'
         },
         {
-          name: 'Forms',
+          name: 'Breaker Size',
           url: '/components/forms',
           icon: 'icon-puzzle'
-        },
-        {
-          name: 'Modals',
-          url: '/components/modals',
-          icon: 'icon-puzzle'
-        },
-        {
-          name: 'Switches',
-          url: '/components/switches',
-          icon: 'icon-puzzle'
-        },
-        {
-          name: 'Tables',
-          url: '/components/tables',
-          icon: 'icon-puzzle'
-        },
-        {
-          name: 'Tabs',
-          url: '/components/tabs',
-          icon: 'icon-puzzle'
         }
       ]
     },
-    {
-      name: 'Icons',
-      url: '/icons',
-      icon: 'icon-star',
-      children: [
-        {
-          name: 'Font Awesome',
-          url: '/icons/font-awesome',
-          icon: 'icon-star',
-          badge: {
-            variant: 'secondary',
-            text: '4.7'
-          }
-        },
-        {
-          name: 'Simple Line Icons',
-          url: '/icons/simple-line-icons',
-          icon: 'icon-star'
-        }
-      ]
-    },
-    {
-      name: 'Widgets',
-      url: '/widgets',
-      icon: 'icon-calculator',
-      badge: {
-        variant: 'info',
-        text: 'NEW'
-      }
-    },
-    {
-      name: 'Charts',
-      url: '/charts',
-      icon: 'icon-pie-chart'
-    },
+
+    
     {
       divider: true
     },
-    {
-      title: true,
-      name: 'Extras',
-    },
-    {
-      name: 'Pages',
-      url: '/pages',
-      icon: 'icon-star',
-      children: [
-        {
-          name: 'Login',
-          url: '/pages/login',
-          icon: 'icon-star'
-        },
-        {
-          name: 'Register',
-          url: '/pages/register',
-          icon: 'icon-star'
-        },
-        {
-          name: 'Error 404',
-          url: '/pages/404',
-          icon: 'icon-star'
-        },
-        {
-          name: 'Error 500',
-          url: '/pages/500',
-          icon: 'icon-star'
-        }
-      ]
-    },
+    // {
+    //   title: true,
+    //   name: 'Projects',
+    // },
+    // {
+    //   name: 'Pages',
+    //   url: '/pages',
+    //   icon: 'icon-star',
+    //   children: [
+    //     {
+    //       name: 'Login',
+    //       url: '/pages/login',
+    //       icon: 'icon-star'
+    //     },
+    //     {
+    //       name: 'Register',
+    //       url: '/pages/register',
+    //       icon: 'icon-star'
+    //     },
+    //     {
+    //       name: 'Error 404',
+    //       url: '/pages/404',
+    //       icon: 'icon-star'
+    //     },
+    //     {
+    //       name: 'Error 500',
+    //       url: '/pages/500',
+    //       icon: 'icon-star'
+    //     }
+    //   ]
+    // },
     {
       name: 'Voltage Drop',
       url: 'http://dkitaw.com',
