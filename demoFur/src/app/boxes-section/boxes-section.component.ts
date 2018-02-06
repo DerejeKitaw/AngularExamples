4700380366efb42f045eb8980890ff5b29d42e43
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-05 21:57:56 -0500

    boxes component added

---
+++ demoFur/src/app/boxes-section/boxes-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boxes-section',
+  templateUrl: './boxes-section.component.html',
+  styleUrls: ['./boxes-section.component.scss']
+})
+export class BoxesSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
