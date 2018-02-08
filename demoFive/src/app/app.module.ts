d8a18dee12a29c0e7cab4cb3e67d3916226bf428
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-08 00:08:25 -0500

    register component added

--- demoFive/src/app/app.module.ts
+++ demoFive/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserAnimationsModule,
