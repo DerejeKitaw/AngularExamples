154092c2afe780b92a8bc4bc88faaf9c3f921f69
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-09 20:04:36 -0500

    recover form updated

--- demoFive/src/app/auth/recover/recover.component.ts
+++ demoFive/src/app/auth/recover/recover.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-recover',
   templateUrl: './recover.component.html',
   styleUrls: ['./recover.component.scss']
 })
 export class RecoverComponent implements OnInit {
+  public recoverForm: FormGroup;
+  public auth: AuthService;
 
-  constructor() { }
+  constructor(auth: AuthService) {
+    this.auth = auth;
+  }
 
   ngOnInit() {
+    this.recoverForm = new FormGroup({
+      email: new FormControl(),
+    });
   }
 
 }
