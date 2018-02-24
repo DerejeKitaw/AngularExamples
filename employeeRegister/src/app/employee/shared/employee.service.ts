cd61d600851c99a172ec91e34fd80882040236fd
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-24 03:37:35 -0500

    employee service implemented

--- employeeRegister/src/app/employee/shared/employee.service.ts
+++ employeeRegister/src/app/employee/shared/employee.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
 
+// Firebase connections
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+import { Employee } from './employee.model';
+
 @Injectable()
 export class EmployeeService {
+  employeeList: AngularFireList<any>;
+  selectedEmployee: Employee = new Employee();
 
-  constructor() { }
+  constructor(private firebase: AngularFireDatabase) {}
 
+  getAllEmployee() {
+    this.employeeList = this.firebase.list('employee');
+    return this.employeeList;
+  }
+
+  insertEmployee(employee: Employee) {
+    this.employeeList.push({
+      name: employee.name,
+      position: employee.position,
+      office: employee.office,
+      salary: employee.salary
+    });
+  }
+  updateEmployee(employee: Employee) {
+    this.employeeList.update(employee.$key, {
+      name: employee.name,
+      position: employee.position,
+      office: employee.office,
+      salary: employee.salary
+    });
+  }
+  deleteEmployee($key: string) {
+    this.employeeList.remove($key);
+  }
 }
