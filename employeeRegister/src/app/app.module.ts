860184e16fb26d74ea2379f98052270ecf41a8a8
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-24 03:27:42 -0500

    firebase included in the app

--- employeeRegister/src/app/app.module.ts
+++ employeeRegister/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+// firebase import
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
