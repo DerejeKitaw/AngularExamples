e62cae8e8ac33bd057810c308028d8b834a5e140
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-24 00:39:54 -0500

    TodoService provided

--- todolistapp/src/app/app.module.ts
+++ todolistapp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { environment } from '../environments/environment';
 import { TodoComponent } from './todo/todo.component';
+import { TodoService } from './todo.service';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { TodoComponent } from './todo/todo.component';
     AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
