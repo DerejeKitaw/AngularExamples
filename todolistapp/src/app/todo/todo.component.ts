18e9a00bd4fe377bd9ab2b3a5494b5358942af8d
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-24 01:36:50 -0500

    get todo list when component start

--- todolistapp/src/app/todo/todo.component.ts
+++ todolistapp/src/app/todo/todo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+  toDoListArray: any[];
 
-  constructor() { }
+  constructor(private toDoService: TodoService) { }
 
   ngOnInit() {
+    // Get to do list when component initialized
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(y => {
+        const x = y.payload.toJSON();
+        x['$key'] = y.key;
+        this.toDoListArray.push(x);
+      });
+      // sort array
+      this.toDoListArray.sort((a , b) => {
+        return a.isChecked - b.isChecked;
+      });
+    });
+  }
+  onAdd(itemTitle) {
+    this.toDoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
   }
 
 }
