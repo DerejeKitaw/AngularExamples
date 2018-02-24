266aa18e1ae3819b5d8f3acf976b9b8a9fc5f5f2
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-24 00:32:46 -0500

    todo service implemented

--- todolistapp/src/app/todo.service.ts
+++ todolistapp/src/app/todo.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
 
+// Firebase connections
+import { AngularFireDatabase , AngularFireList} from 'angularfire2/database';
+
 @Injectable()
 export class TodoService {
+// toDoList returned by Firebase is an array.
+// So all array properties work on todoList
+toDoList: AngularFireList<any>;
 
-  constructor() { }
+  constructor(private firebasedb: AngularFireDatabase) { }
 
+  // To get todo list from firebase
+  getToDoList() {
+    this.toDoList = this.firebasedb.list('title');
+    return this.toDoList;
+  }
+
+  // To add todo to firebase
+  addTitle(title: string) {
+    this.toDoList.push({
+      title: title,
+      isChecked: false
+    });
+  }
+
+  // To check or unchecked title in firebase
+  checkOrUncheckTitle($key: string, flag: boolean) {
+    this.toDoList.update($key, { isChecked: flag });
+  }
+
+  // To delete or remove title from todo list
+  removeTitle($key: string) {
+    this.toDoList.remove($key);
+  }
 }
